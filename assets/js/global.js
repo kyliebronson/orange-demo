8040c085dc550cb74b3ec0fa3844ae1f663f4172
Kylie Bronson <kyliebronson@Kylies-Air-2.attlocal.net>
2021-08-11 17:12:44 -0400

    my first website

---
+++ assets/js/global.js
@@ -0,0 +1,30 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+    // init
+    let cookiesBanner= document.querySelector('.cookies-banner') 
+    let cookiesBannerCloseButton = document.querySelector('.cookies-banner-button .button')
+    let cookiesBannerDelay = 2000
+       
+        if (cookiesBanner) {
+           setTimeout(showBanner, cookiesBannerDelay); 
+       }
+
+       // events
+       if (cookiesBannerCloseButton) {
+           cookiesBannerCloseButton.addEventListener('click', cookiesBannerCloseButtonClicked) 
+
+       }
+       
+       // functions 
+       function cookiesBannerCloseButtonClicked(e) {
+           e.preventDefault()
+           hideBanner()
+       }
+       function showBanner() {
+        cookiesBanner.classList.add('show') 
+        
+       }
+       function hideBanner() {
+           cookiesBanner.classList.remove('show')
+       }
+   }) 
